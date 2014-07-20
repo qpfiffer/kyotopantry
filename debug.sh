a810e45a0bc2381c0f29c656c028facdba48416d
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-07-20 12:16:54 -0700

    Added debug script so I'm not testing my deduplication program on OlegDB/*

---
+++ debug.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+DDIRECTORY=/tmp/kyotopantry_test/
+
+mkdir -p $DDIRECTORY
+
+echo 'test1' > $DDIRECTORY/test1
+echo 'test2' > $DDIRECTORY/test2
+echo 'test3' > $DDIRECTORY/test3
+echo 'test4' > $DDIRECTORY/test4
+
+tar -czf $DDIRECTORY/tarball_a.tar.gz $DDIRECTORY/test*
+
+echo 'test5' > $DDIRECTORY/test5
+
+tar -czf $DDIRECTORY/tarball_b.tar.gz $DDIRECTORY/test*
+
+gdb --args ./kyotopantry $DDIRECTORY./tarball_*.tar.gz
